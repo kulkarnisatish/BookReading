b44161ca6647f8829d04de11dfc1c54127e1b2ce
sk017843 <satish.kulkarni2@cerner.com>
2021-05-07 15:53:45 +0530

    Max element using recursion

--- GrokkingAlgorithms/src/main/java/org/example/App.java
+++ GrokkingAlgorithms/src/main/java/org/example/App.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import java.util.ArrayList;
+
 /**
  * Hello world!
  *
@@ -8,6 +10,20 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.print(RecursionCountElements.CountElement(new int[]{1}, 0, 1));
+        if(args.length == 0) args= new String[]{"2","3","5","9"};
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i = 0; i < Math.min(5, args.length); i++)
+            list.add(Integer.parseInt(args[i]));
+        //System.out.print(RecursionCountElements.CountElement(new int[]{1}, 0, 1));
+        System.out.print(RecursionCountElements.MaxElements(convertIntegers(list), list.toArray().length));
+    }
+    public static int[] convertIntegers(ArrayList<Integer> integers)
+    {
+        int[] ret = new int[integers.size()];
+        for (int i=0; i < ret.length; i++)
+        {
+            ret[i] = integers.get(i).intValue();
+        }
+        return ret;
     }
 }
--- GrokkingAlgorithms/src/main/java/org/example/RecursionCountElements.java
+++ GrokkingAlgorithms/src/main/java/org/example/RecursionCountElements.java
@@ -14,4 +14,18 @@ public class RecursionCountElements {
             return 1 + CountElement(arr, sindex +1, lindex);
         }
     }
+
+    public static int MaxElements(int[]arr, int n)
+    {
+        if(n ==1)
+        {
+            return arr[0];
+        }else
+        {
+            if(arr[n-1] > MaxElements(arr, n-1))
+                return arr[n-1];
+            else
+                return MaxElements(arr, n-1);
+        }
+    }
 }
